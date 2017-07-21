cc7ccdc904ce91c562316b93ff345e843736a3ad
Rodion Zlobin <rodion.zlobin@so4it.com>
2017-07-21 16:00:52 +0200

    userentity

--- silvermill-data-entity/src/main/java/com/rodion/silvermilldata/entity/UserEntity.java
+++ silvermill-data-entity/src/main/java/com/rodion/silvermilldata/entity/UserEntity.java
@@ -4,4 +4,28 @@ package com.rodion.silvermilldata.entity;
  * @author Rodion Zlobin {@literal <mailto:rodion.zlobin@so4it.com/>}.
  */
 public class UserEntity {
+
+    private String username;
+    private String password;
+
+    public UserEntity(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
